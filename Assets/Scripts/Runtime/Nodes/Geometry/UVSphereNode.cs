af18b97ef9330eaf270b3bc9b987762a2deecabe
kazaxc <lgkscast@gmail.com>
2022-12-13 04:14:41 +0000

    Added specification for the node

--- Assets/Scripts/Runtime/Nodes/Geometry/UVSphereNode.cs
+++ Assets/Scripts/Runtime/Nodes/Geometry/UVSphereNode.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 namespace MiniDini.Nodes
 {
     /// <summary>
-    /// <see cref="Node"/> that has a list of children.
+    /// <see cref="Node"/> This is the UV sphere node it is used to creat a sphere.
+    /// The sphere is created by breaking it down into two attributes the segments and the rings.
+    /// The points are then placed on the sphere using the formula: x = r * sin(theta) * cos(phi), y = r * cos(theta), z = r * sin(theta) * sin(phi)
+    /// Where theta is the angle around the y axis and phi is the angle around the x axis.
+    /// The points are then connected to create the quads which make up the prims of the sphere.
     /// </summary>
     [System.Serializable]
     public class UVSphereNode : Node
