733337f2f5e9d9ba2243e653fcd945c4e11cff44
Kallum <lgkscast@gmail.com>
2023-01-04 15:54:35 +0000

    Update UVSphereNode.cs
    Removed Solution for people to complete themselves

--- Assets/Scripts/Runtime/Nodes/Geometry/UVSphereNode.cs
+++ Assets/Scripts/Runtime/Nodes/Geometry/UVSphereNode.cs
@@ -43,50 +43,6 @@ namespace MiniDini.Nodes
             m_geometry.Empty();
 
             // here is where we construct the geometry for the UV sphere
-            // starting with the points
-
-            // create a list of points
-            int[] points = new int[segments * rings];
-
-            for (int i = 0; i < segments; i++)
-            {
-                for (int j = 0; j < rings; j++)
-                {
-                    // create a point
-                    Point point = new();
-
-                    // calculate the angle for the point
-                    float theta = (float)i / (float)(segments - 1) * Mathf.PI;
-                    float phi = (float)j / (float)(rings - 1) * Mathf.PI * 2.0f;
-
-                    // calculate the position of the point
-                    point.position = new Vector3(radius * Mathf.Sin(theta) * Mathf.Cos(phi), radius * Mathf.Cos(theta), radius * Mathf.Sin(theta) * Mathf.Sin(phi));
-
-                    // add the point to the geometry
-                    points[i * segments + j] = m_geometry.AddPoint(point);
-                }
-            }
-
-            // now create the prims
-            for (int i = 0; i < segments - 1; i++)
-            {
-                for (int j = 0; j < segments - 1; j++)
-                {
-                    // create a quad
-                    Prim quad = new();
-
-                    quad.points.Add(points[i * segments + j]);
-                    quad.points.Add(points[i * segments + j + 1]);
-                    quad.points.Add(points[(i + 1) * segments + j + 1]);
-                    quad.points.Add(points[(i + 1) * segments + j]);
-
-                    m_geometry.AddPrim(quad);
-                }
-            }
-
-            return m_geometry;
-        }
-
         #endregion
     }
 }
