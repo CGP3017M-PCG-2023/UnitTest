6bc4b5db6933ff9bce6867980f14c3239705f8c3
RROUOL <79855173+RROUOL@users.noreply.github.com>
2023-01-09 23:21:56 +0000

    Added search + get functionality to Geometry.cs
    extra functionality which allows for easier completion of the grid, select & blast nodes:
    
    Vector3 GetPoint(int index)
    int FindPoint(Vector3 pos, bool addPointIfNull)
    List<Vector3> getPointList()
    List<Prim> getPrimList()
    void OrphanPoint(int index)
    void RemoveOrphanedPoint(int index)
    int getPrimsAttachedToPoint(int index)

--- Assets/Scripts/Runtime/Geometry/Geometry.cs
+++ Assets/Scripts/Runtime/Geometry/Geometry.cs
@@ -59,6 +59,42 @@ namespace MiniDini
 			prims.Clear();
 		}
 
+		public Vector3 GetPoint(int index)
+		{
+			return points[index].position;
+		}
+
+		public virtual int FindPoint(Vector3 pos, bool addPointIfNull)
+		{
+			for (int i = 0; i < points.Count; i += 1)
+			{
+				if (points[i].position == pos)
+				{
+					return i;
+				}
+			}
+
+			//Debug.Log("DIDN'T FIND THE POINT - " + pos);
+			
+			if (addPointIfNull)
+			{
+				Point point = new();
+
+				point.position = pos;
+
+				int j = AddPoint(point);
+
+				//Debug.Log("GIVING " + pos + " AS NEW INDEX " + j);
+
+				return j;
+			}
+			else
+			{
+				return -1;
+			}
+
+		}
+
 		public List<Vector3> getPointList()
 		{
 			List<Vector3> mypoints = new List<Vector3>();
@@ -68,6 +104,87 @@ namespace MiniDini
 			}
 			return mypoints;
 		}
+
+		public List<Prim> getPrimList()
+		{
+			List<Prim> myprims = new List<Prim>();
+			foreach(Prim p in prims)
+			{
+				myprims.Add(p);
+			}
+			return myprims;
+		}
+		
+		public void OrphanPoint(int index)
+		{
+			for (int i = 0; i < prims.Count; i += 1)
+			{
+				for (int j = 0; j < prims[i].points.Count; j += 1)
+				{
+					if (index == prims[i].points[j])
+					{
+						Debug.Log("OrphanPoint removal");
+						prims[i].selected = true;
+						prims[i].points.RemoveAt(j);
+						j -= 1;
+					}
+				}
+			}
+		}
+
+		public void RemoveOrphanedPoint(int index)
+		{
+			Debug.Log("Orphaned point index #" + index + " removed. Adjusting");
+			points.RemoveAt(index);
+
+			for (int i = 0; i < prims.Count; i += 1)
+			{
+				for (int j = 0; j < prims[i].points.Count; j += 1)
+				{
+					//if (prims[i].points[j] == index)
+					//{
+					//	Debug.Log("Point index #" + j + " of prim #" + i + " is the same index as " + index + " (was " + prims[i].points[j] + "). Removing");
+					//	prims[i].points.RemoveAt(j);
+					//	prims[i].positions.RemoveAt(j);
+					//	j -= 1;
+					//}
+					
+					if (prims[i].points[j] >= index)
+					{
+						Debug.Log("Point index #" + j + " of prim #" + i + " adjusted to " + (prims[i].points[j]-1) + " (was " + prims[i].points[j] + ")");
+						prims[i].points[j] -= 1;
+					}
+
+					// removes point if point index becomes less than 0 - this shouldn't be possible, but who knows
+					//if (prims[i].points[j] < 0)
+					//{
+					//	prims[i].points.RemoveAt(j);
+					//	j -= 1;
+					//}
+				}
+			}
+		}
+
+		public int getPrimsAttachedToPoint(int index)
+		{
+			int count = 0;
+
+			for (int i = 0; i < prims.Count; i += 1)
+			{
+				//Debug.Log("Checking prim " + i);
+				for (int j = 0; j < prims[i].points.Count; j += 1)
+				{
+					//Debug.Log("Checking point " + j + " of prim " + i);
+					if (index == prims[i].points[j])
+					{
+						Debug.Log(index + " is used for point " + j + " of prim " + i);
+						count += 1;
+					}
+				}
+			}
+
+			return count;
+		}		
 
 		// pac note - C# has issues with deep copies of classes, see ICloneable and the like.. basically it uses pass by reference
 		// and that royally messes with copies of data within classes (because you get one instance messing with data referenced by another)
